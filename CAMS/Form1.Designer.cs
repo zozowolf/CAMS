d69a2b2eaff8a70a40a4db19486856772c40e4e5
zozowolf <enzolafouine2004@gmail.com>
2024-01-09 11:24:53 +0100

    affichage graph
    prise en main de C# avec un affichage d'un graph a données brut dans le code

---
+++ CAMS/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace CAMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblDateTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Unscaled;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(9, 22);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.RoyalBlue;
+            series1.IsVisibleInLegend = false;
+            series1.LabelForeColor = System.Drawing.Color.Bisque;
+            series1.Legend = "Legend1";
+            series1.Name = "Valeur";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(546, 203);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // lblDateTime
+            // 
+            this.lblDateTime.AccessibleName = "lblDateTime";
+            this.lblDateTime.AutoSize = true;
+            this.lblDateTime.Location = new System.Drawing.Point(534, 584);
+            this.lblDateTime.Name = "lblDateTime";
+            this.lblDateTime.Size = new System.Drawing.Size(70, 13);
+            this.lblDateTime.TabIndex = 1;
+            this.lblDateTime.Text = "heure et date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 642);
+            this.Controls.Add(this.lblDateTime);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lblDateTime;
+    }
+}
+
---
+++ CAMS/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CAMS
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+            InitializeTimer();
+        }
+
+        private void InitializeChart()
+        {
+            // Données brut
+
+            int[] valeur = { 8, 6, 7, 5, 9, 8, 4, 7, 8, 1, 6, 3, 7, 2, 4, 0 }; // Exemple de données 
+
+
+            // Ajouter les données au graphique
+            for (int i = 0; i < valeur.Length; i++)
+            {
+                chart1.Series["Valeur"].Points.AddXY("", valeur[i]);
+            }
+
+            // Configuration du graphique
+            chart1.Series["Valeur"].ChartType = SeriesChartType.Column;
+
+            // Facultatif : ajuster les limites de l'axe Y
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 10;
+
+            // Rendre les valeurs des axes invisibles
+            chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = false;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+            chart1.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
+
+            // Rendre le fond du graphique transparent
+            chart1.BackColor = Color.Transparent;
+            chart1.ChartAreas[0].BackColor = Color.Transparent;
+
+            // Rendre le quadrillage invisible
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            // Changer la couleur de la série à rouge
+            chart1.Series["Valeur"].Color = Color.Red;
+
+            // Ajouter un titre au graphique
+            Title title = new Title("Ch : 1");
+            title.Font = new Font("Arial", 18, FontStyle.Regular); // Définir la police à Arial, taille 18
+            title.Alignment = ContentAlignment.TopLeft; // Aligner en haut à gauche
+            title.ForeColor = Color.Red; // Changer la couleur du titre en rouge
+            chart1.Titles.Add(title);
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void InitializeTimer()
+        {
+            // Initialiser le timer
+            timer = new Timer();
+            timer.Interval = 1000; // Intervalle en millisecondes (1 seconde)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Mettre à jour le label avec l'heure et la date actuelles à chaque tick de timer
+            lblDateTime.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
