0a01b8557c2606ad7c5eb546190d0a3660e7529c
Enzo <enzolafouine2004@gmail.com>
2024-02-15 14:41:04 +0100

    BD lié a actogram page

--- application/application/actogramPage.cs
+++ application/application/actogramPage.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Data.SqlClient; // SQL Server local DB
 
 namespace application
 {
@@ -11,7 +17,8 @@ namespace application
     */
     public partial class actogramPage : Form
     {
-        private const int NumCharts = 8;
+        
+
         private const int MaxXValue = 1440;
         private const int ChartHeight = 25;
         private const int Margin = 10;
@@ -29,9 +36,9 @@ namespace application
             // Créer les graphiques à lignes avec des points
             Random random = new Random(); // Générateur de nombres aléatoires
 
-            int chartHeight = (groupBox1.Height - (NumCharts + 1) * Margin) / (NumCharts + 1); // Calculer la hauteur des graphiques
+            int chartHeight = (groupBox1.Height - (NumCharts() + 1) * Margin) / (NumCharts() + 1); // Calculer la hauteur des graphiques
 
-            for (int i = 1; i <= NumCharts; i++)
+            for (int i = 1; i <= NumCharts(); i++)
             {
                 Chart chart = CreateChart(i, random, chartHeight);
                 groupBox1.Controls.Add(chart);
@@ -49,11 +56,7 @@ namespace application
 
             Series series = chart.Series.Add($"Chart{chartNumber}");
 
-            for (int x = 0; x <= MaxXValue; x++)
-            {
-                int randomValue = random.Next();
-                series.Points.AddXY(x, randomValue);
-            }
+            UpdateChart(chart, chartNumber);
 
             // Configurer le style de la ligne
             series.Color = Color.Green; // Couleur verte
@@ -63,6 +66,85 @@ namespace application
 
             return chart;
         }
+
+
+        private int NumCharts()
+        {
+            string cn_string = Properties.Settings.Default.DBCAMSConnectionString;
+            HashSet<DateTime> joursDifferents = new HashSet<DateTime>();
+
+            using (SqlConnection cn_connection = new SqlConnection(cn_string))
+            {
+                cn_connection.Open();
+                string sql_Text = $"SELECT dateHeure FROM Mesure WHERE IdChannel = 1 ORDER BY dateHeure ASC";
+
+                using (SqlCommand cmd = new SqlCommand(sql_Text, cn_connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader["dateHeure"] != DBNull.Value)
+                            {
+                                DateTime dateHeure = (DateTime)reader["dateHeure"];
+                                // Ignorer les composants d'heure pour ne considérer que la date
+                                DateTime jourUnique = dateHeure.Date;
+                                joursDifferents.Add(jourUnique);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return joursDifferents.Count;
+        }
+
+        private void UpdateChart(Chart chart, int chartNumber)
+        {
+            // Réinitialiser les valeurs (temporaire en attendant la BD)
+            chart.Series[$"Chart{chartNumber}"].Points.Clear();
+
+            string cn_string = Properties.Settings.Default.DBCAMSConnectionString;
+            using (SqlConnection cn_connection = new SqlConnection(cn_string))
+            {
+                cn_connection.Open();
+
+                // Initialiser un tableau pour stocker les valeurs par minute
+                double[] valeursParMinute = new double[MaxXValue + 1];
+
+                string sql_Text = $"SELECT valeur, dateHeure FROM Mesure WHERE IdChannel = 1 ORDER BY dateHeure ASC";
+                using (SqlCommand cmd = new SqlCommand(sql_Text, cn_connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Utiliser la colonne correspondante pour récupérer les données
+                            double valeur = Convert.ToDouble(reader["valeur"]);
+                            DateTime dateHeure = Convert.ToDateTime(reader["dateHeure"]);
+
+                            // Calculer l'index de la minute dans la journée
+                            int minuteIndex = dateHeure.Hour * 60 + dateHeure.Minute;
+
+                            // Stocker la valeur dans le tableau
+                            valeursParMinute[minuteIndex] = valeur;
+                        }
+                    }
+                }
+
+                // Ajouter les valeurs au graphique
+                for (int x = 0; x <= MaxXValue; x++)
+                {
+                    double valeur = valeursParMinute[x];
+                    DataPoint dataPoint = new DataPoint();
+                    dataPoint.SetValueXY(x, valeur);
+                    chart.Series[$"Chart{chartNumber}"].Points.Add(dataPoint);
+                }
+            }
+        }
+
+
+
 
         private Chart CreateBlueLineChart(int chartHeight)
         {
@@ -91,7 +173,7 @@ namespace application
             series.Color = Color.Blue; // Couleur bleue
             series.BorderWidth = 2;
 
-            ConfigureChart(chart, NumCharts + 1, chartHeight);
+            ConfigureChart(chart, NumCharts() + 1, chartHeight);
 
             return chart;
         }
@@ -99,6 +181,10 @@ namespace application
 
         private void ConfigureChart(Chart chart, int chartNumber, int chartHeight)
         {
+            // Ajuster les limites des l'axes 
+            chart.ChartAreas[0].AxisX.Minimum = 0;
+            chart.ChartAreas[0].AxisX.Maximum = MaxXValue;
+
             // Rendre les valeurs des axes invisibles
             chart.ChartAreas[0].AxisX.LabelStyle.Enabled = false;
             chart.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
